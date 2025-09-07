1f25ef41b227f2f2f583cf9f2eb46707a07c34ab
ali <ali.page-ouarti@epitech.eu>
2025-09-07 02:53:36 +0200

    fix : removing errors in the .h

--- build/CMakeFiles/Real-Time-Voxel-Path-Tracer.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for Real-Time-Voxel-Path-Tracer.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Real-Time-Voxel-Path-Tracer.dir/src/Camera.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Real-Time-Voxel-Path-Tracer.dir/src/Camera.cpp.o: \
- /home/ali/Desktop/Real-Time-Voxel-Path-Tracer/src/Camera.cpp \
- /usr/include/stdc-predef.h
